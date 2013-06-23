acb684f4ea45e3d26ac2662c450ad35a19ff3426
Jerome Maye <jerome.maye@mavt.ethz.ch>
2013-06-23 00:19:28 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+rqt_bag_plugins_snappy
+================================
+
+RQT plugins for snappy messages.
---
+++ src/rqt_bag_plugins_snappy/python/lib/rqt_bag_plugins_snappy/plugins.py
@@ -0,0 +1,24 @@
+################################################################################
+# Copyright (C) 2013 by Jerome Maye                                            #
+# jerome.maye@gmail.com                                                        #
+#                                                                              #
+# This program is free software; you can redistribute it and/or modify         #
+# it under the terms of the Lesser GNU General Public License as published by  #
+# the Free Software Foundation; either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# Lesser GNU General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the Lesser GNU General Public License     #
+# along with this program. If not, see <http://www.gnu.org/licenses/>.         #
+################################################################################
+
+from image_snappy_timeline_renderer import ImageSnappyTimelineRenderer
+from image_snappy_view import ImageSnappyView
+
+def get_rqt_bag_plugins():
+  return [(ImageSnappyView, ImageSnappyTimelineRenderer,
+    ['mv_cameras/ImageSnappyMsg'])]
